101684b43ffce617681ca92ef5836b6010144e76
youngwishes <mysc1@yandex.ru>
2023-11-06 18:43:16 +0300

    Base router prefix added

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from src.core.users.api import router as user_router
+from fastapi import FastAPI, Depends
+from src.core.users.auth import auth_router, register_router, fastapi_users
+from src.core.users.models import User
+from src.core.config import get_settings
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
+settings = get_settings()
 
 
-app = FastAPI(debug=True, description="Diploma")
+class YshopAPI(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.router.prefix = settings.BASE_API_PREFIX
 
-app.include_router(user_router, tags=["users"])
+
+app = YshopAPI(
+    debug=settings.DEBUG,
+    description="Online shop",
+    title=settings.PROJECT_NAME,
+)
+
+app.include_router(auth_router, tags=["/auth"], prefix="/auth/jwt")
+app.include_router(register_router, tags=["/auth"], prefix="/auth")
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+async def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
 
 
 @app.get("/")
