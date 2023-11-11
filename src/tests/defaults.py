1c7aff1c855c545cd2e0a0c61e1e06ee437c61b4
youngwishes <d.fedorov@ylab.team>
2023-11-11 22:55:18 +0300

    Вынес тестовые константы

--- src/tests/defaults.py
+++ src/tests/defaults.py
@@ -1,3 +1,17 @@
-SQLALCHEMY_DATABASE_TEST_URI = "postgresql+asyncpg://Yshop:Yshop@yshop-postgres/test_db"
+TEST_DB_NAME = "test_db"
 
-BASE_API_URL = "http://0.0.0.0:8000/api/v1"
+SQLALCHEMY_DATABASE_TEST_URI = (
+    f"postgresql+asyncpg://Yshop:Yshop@yshop-postgres/{TEST_DB_NAME}"
+)
+
+HOST_URL = "http://0.0.0.0:8000/"
+
+TEST_USER_EMAIL = "test@example.com"
+
+TEST_USER_FIRST_NAME = "test_first_name"
+
+TEST_USER_LAST_NAME = "test_user_last_name"
+
+TEST_USER_MIDDLE_NAME = "test_user_middle_name"
+
+TEST_USER_PASSWORD = "test_user_password"
