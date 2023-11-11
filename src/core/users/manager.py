5f60e759f1d3b8ea6b0a86183cdbd62e58cdef35
youngwishes <d.fedorov@ylab.team>
2023-11-11 22:58:17 +0300

    Убрал лишние методы в UserManager

--- src/core/users/manager.py
+++ src/core/users/manager.py
@@ -1,11 +1,17 @@
-from typing import Optional
-from fastapi import Request
-from fastapi_users import BaseUserManager, IntegerIDMixin, models
-from starlette.responses import Response
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from fastapi_users import BaseUserManager, IntegerIDMixin
 from src.core.users.models import User
 from src.core.config import get_settings
 from src.core.users.schemas import UserUpdate
-from datetime import datetime
+
+
+if TYPE_CHECKING:
+    from fastapi import Request
+    from starlette.responses import Response
+    from fastapi_users.models import UP
 
 
 settings = get_settings()
@@ -15,32 +21,13 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = settings.SECRET_KEY
     verification_token_secret = settings.SECRET_KEY
 
-    async def on_after_register(self, user: User, request: Optional[Request] = None):
-        print(f"User {user.id} has registered.")
-
-    async def on_after_forgot_password(
-        self,
-        user: User,
-        token: str,
-        request: Optional[Request] = None,
-    ):
-        print(f"User {user.id} has forgot their password. Reset token: {token}")
-
-    async def on_after_request_verify(
-        self,
-        user: User,
-        token: str,
-        request: Optional[Request] = None,
-    ):
-        print(f"Verification requested for user {user.id}. Verification token: {token}")
-
     async def on_after_login(
         self,
-        user: models.UP,
-        request: Optional[Request] = None,
-        response: Optional[Response] = None,
+        user: UP,
+        request: Request | None = None,
+        response: Response | None = None,
     ) -> None:
-        update_json = await user.to_json()
+        update_json = user.to_json()
         update_json["last_login"] = datetime.now()
 
         await self.update(UserUpdate(**update_json), user, safe=True, request=request)
