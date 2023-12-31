9d86050da923bf975b77a5a0b9613dd9f7649690
viru <isergix9@gmail.com>
2023-12-31 17:58:39 +0500

    Tests fix after conflicts

--- src/tests/users/test_users_views.py
+++ src/tests/users/test_users_views.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
+
 from typing import TYPE_CHECKING, Sequence
+
 import pytest
 from fastapi import status
 from sqlalchemy import select
 
-from src.tests import defaults
-from src.tests.helpers import get_objects_count, get_object, check_object_data
 from src.core.users.models import User
 from src.main import app
+from src.tests import defaults
+from src.tests.helpers import get_objects_count, get_object, check_object_data
 
 if TYPE_CHECKING:
     from httpx import AsyncClient
@@ -143,8 +145,13 @@ async def test_edit_user_by_authorized(
 
 
 @pytest.mark.anyio
-async def test_edit_user_by_unauthorized(async_client: AsyncClient):
-    """Тест на обновление данных пользователя от имени не авторизованного пользователя"""
+async def test_edit_user_with_exist_email_by_authorized(
+    authorized_client: AsyncClient,
+    get_test_user_data: dict,
+    session: AsyncSession,
+    create_test_users: Sequence[User],
+):
+    """Тест обновления данных пользователя на существующие от имени авторизированного пользователя"""
     url = app.url_path_for("user_edit")
     get_test_user_data["email"] = create_test_users[1].email
     response = await authorized_client.patch(url, json=get_test_user_data)
@@ -159,8 +166,23 @@ async def test_edit_user_by_unauthorized(async_client: AsyncClient):
 
 
 @pytest.mark.anyio
-async def test_edit_user_by_unauthorized(async_client: AsyncClient):
-    """Тест проверяет редактирование пользователя от имени не авторизированного пользователя"""
+async def test_edit_user_by_unauthorized(
+    async_client: AsyncClient,
+    get_test_user_data: dict,
+    session: AsyncSession,
+):
+    """Тест на обновление данных пользователя от имени не авторизованного пользователя"""
     url = app.url_path_for("user_edit")
-    response = await async_client.patch(url)
+    to_change_email = "example_user_email_test1@example.com"
+    get_test_user_data["email"] = to_change_email
+    user_stmt = await session.execute(
+        select(User).where(User.email == to_change_email),
+    )
+    assert user_stmt.unique().scalar_one_or_none() is None
+    response = await async_client.patch(url, json=get_test_user_data)
+
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    user_stmt = await session.execute(
+        select(User).where(User.email == to_change_email),
+    )
+    assert user_stmt.unique().scalar_one_or_none() is None
